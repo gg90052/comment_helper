8a7c69d8b60f05b587217a384bf59d44c6588ca5
Teddy <>
2023-06-06 13:01:37 +0800

    remove youtube data api

--- src/components/mainArea/YtVideo.vue
+++ src/components/mainArea/YtVideo.vue
@@ -5,28 +5,10 @@
 </template>
 
 <script setup lang="ts">
-const showVideo = ref(false);
-const videoURL = ref('https://www.youtube.com/embed/dCn8UppZGOo');
-const CHANNEL_ID = 'UCNLxbtdTe-fFl8uDUS6tMcw';
-const YOUTUBE_API_KEY = process.env.VUE_APP_YOUTUBE_API_KEY;
+const showVideo = ref(true);
+const VIDEO_ID = 'dCn8UppZGOo';
+const videoURL = ref('https://www.youtube.com/embed/' + VIDEO_ID);
 
-const getVideo = () => {
-  fetch(`https://www.googleapis.com/youtube/v3/search?part=snippet&channelId=${CHANNEL_ID}&maxResults=1&order=date&key=${YOUTUBE_API_KEY}`)
-  .then(response => response.json())
-  .then(data => {
-    // 處理 API 回傳的影片結果
-    const videos = data.items;
-    videos.forEach(video => {
-      const videoId = video.id.videoId;
-      videoURL.value = `https://www.youtube.com/embed/${videoId}`;
-      showVideo.value = true;
-    });
-  })
-  .catch(error => {
-    console.error('發生錯誤：', error);
-  });
-}
-getVideo();
 
 const setVideoShow = (show: boolean) => {
   showVideo.value = show;
