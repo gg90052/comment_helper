c7c54fb56e34b2cca38fbbe5d0916b7de96c3628
Teddy <>
2023-06-13 16:06:52 +0800

    change yt video

--- src/components/mainArea/YtVideo.vue
+++ src/components/mainArea/YtVideo.vue
@@ -1,12 +1,12 @@
 <template>
-  <div :class="showVideo ? '':'hidden'">
-    <iframe width="560" height="315" :src="videoURL" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
+  <div :class="showVideo ? '':'hidden'" class="w-full h-0 pb-[56.25%] relative">
+    <iframe class="w-full h-full absolute left-0 top-0" ref="iframe" :src="videoURL" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
   </div>
 </template>
 
 <script setup lang="ts">
 const showVideo = ref(true);
-const VIDEO_ID = 'dCn8UppZGOo';
+const VIDEO_ID = 'kPVZ-6JMpNc';
 const videoURL = ref('https://www.youtube.com/embed/' + VIDEO_ID);
 
 
