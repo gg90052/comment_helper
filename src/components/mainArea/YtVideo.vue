3b4403a31b976f2195c292780fcb63dbac7c55fb
Teddy <>
2023-07-22 23:17:33 +0800

    change yt

--- src/components/mainArea/YtVideo.vue
+++ src/components/mainArea/YtVideo.vue
@@ -1,20 +1,30 @@
 <template>
-  <div :class="showVideo ? '':'hidden'" class="w-full h-0 pb-[56.25%] relative">
-    <iframe class="w-full h-full absolute left-0 top-0" ref="iframe" :src="videoURL" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
+  <div
+    :class="showVideo ? '' : 'hidden'"
+    class="w-full h-0 pb-[56.25%] relative"
+  >
+    <iframe
+      class="w-full h-full absolute left-0 top-0"
+      ref="iframe"
+      :src="videoURL"
+      title="YouTube video player"
+      frameborder="0"
+      allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+      allowfullscreen
+    ></iframe>
   </div>
 </template>
 
 <script setup lang="ts">
 const showVideo = ref(true);
-const VIDEO_ID = 'bajo3dt4LKM';
-const videoURL = ref('https://www.youtube.com/embed/' + VIDEO_ID);
-
+const VIDEO_ID = "7qbixcnJKog";
+const videoURL = ref("https://www.youtube.com/embed/" + VIDEO_ID);
 
 const setVideoShow = (show: boolean) => {
   showVideo.value = show;
-}
+};
 
 defineExpose({
-  setVideoShow
-})
+  setVideoShow,
+});
 </script>
